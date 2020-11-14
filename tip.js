7276ef75563df90bc26f72145103880edaabbf3d
Andrew Schaus <andrewschaus3@gmail.com>
2020-11-13 20:17:28 -0500

    Adding files to repository

---
+++ tip.js
@@ -0,0 +1,37 @@
+
+document.getElementById("calculate").addEventListener("click", bill);
+
+function bill()
+{
+    var BillCost = document.getElementById("cost").value;
+    var numPeople = document.getElementById("people").value;
+    var percent = document.getElementById("percent").value;
+    var cPercent = (document.getElementById("custom").value) * .01;
+
+    if(BillCost == "" || BillCost <=0)
+    {
+        alert("Please enter a price");
+    }
+
+    if(numPeople == ""||numPeople <1)
+    {
+        alert("Please enter a posative integer for number of people")
+    }
+
+    if(cPercent == ""){
+        var tipTotal = (percent * BillCost);
+        var totalcost = (tipTotal - -BillCost);
+    }
+    else{
+        var tipTotal = (cPercent * BillCost);
+        var totalcost = (tipTotal - -BillCost);
+    }
+
+
+
+    var perPerson = totalcost/numPeople
+    perPerson = perPerson.toFixed(2);
+
+    if(BillCost != "" && BillCost >0 && numPeople != "" && numPeople>=1)
+        alert("Each person must pay " + perPerson)
+}
